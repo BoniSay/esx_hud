36289c01849acb3518cfa085b3ce89b843d281dc
Boni <119504919+BoniSay@users.noreply.github.com>
2023-08-18 19:17:54 +0700

    add indonesia

---
+++ locales/id.lua
@@ -0,0 +1,16 @@
+Locales["id"] = {
+    toggleHud = "Beralih HUD on/off",
+    indicatorLeft = "Indikator kiri",
+    indicatorRight = "Indikator kanan",
+    toggleEngine = "Mesin Hidup/Mati",
+    speedChanged = "Kecepatan diubah, kecepatan baru:",
+    resource_not_built = "resource belum dibuat, unduh versi rilisnya, bukan kode sumbernya!",
+    settingsSaveSuccess = "Berhasil menyimpan pengaturan saat ini",
+    settingsResetSuccess = "Pengaturan ulang berhasil",
+    currentVersion = "Versi terbaru",
+    yourVersion = "Versi Kamu:",
+    latestVersion = "Kamu menggunakan versi terbaru!",
+    needUpdateResource = "Kamu perlu mendownload versi terbaru! Saat ini Kamu menggunakan versi lama!",
+    errorGetCurrentVersion = "Terjadi kesalahan saat mencoba mendapatkan versi saat ini!",
+}
+
